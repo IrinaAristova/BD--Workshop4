4c0ffdc51934d2be80ae9945388c4a3fdc1be360
AristovaIrina <43518375+IrinaAristova@users.noreply.github.com>
2020-05-24 13:22:36 +0300

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -1,13 +1,16 @@
+--Умова завдання: Оновлює переможця та країну. Exception: переможця чи країну не знайдено в базі.
 create or replace PROCEDURE ChangeCountry(athletename IN VARCHAR2, countryname IN VARCHAR2)
 IS
     temp VARCHAR2(50);
 BEGIN
+    DBMS_OUTPUT.enable;
     SELECT athlete_name INTO temp FROM Athlete WHERE athlete_name = athletename;
     SELECT country_name INTO temp FROM Country WHERE country_name = countryname;
     UPDATE Athlete
-    SET country_country_name = countryname
+    SET country_name = countryname
     WHERE athlete_name = athletename;
     DBMS_OUTPUT.PUT_LINE('Change atlete country');
 EXCEPTION
     WHEN NO_DATA_FOUND THEN DBMS_OUTPUT.PUT_LINE('Athlete or country not found in database');
 END;
+
