1a0552566a4703586ae62c9cd109f211838a7471
AristovaIrina <43518375+IrinaAristova@users.noreply.github.com>
2020-05-24 13:19:40 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,20 +1,14 @@
 CREATE TABLE athlete (
-    athlete_name          VARCHAR2(50) NOT NULL,
-    gender                VARCHAR2(10),
-    country_country_name  VARCHAR2(50) NOT NULL
+    athlete_name  VARCHAR2(50) NOT NULL,
+    gender        VARCHAR2(10),
+    country_name  VARCHAR2(50) NOT NULL
 );
 
 ALTER TABLE athlete ADD CONSTRAINT athlete_pk PRIMARY KEY ( athlete_name );
 
 CREATE TABLE cities (
-    city_name             VARCHAR2(50) NOT NULL,
-    olimpiada_olimp_year  INTEGER NOT NULL
+    city_name VARCHAR2(50) NOT NULL
 );
-
-CREATE UNIQUE INDEX cities__idx ON
-    cities (
-        olimpiada_olimp_year
-    ASC );
 
 ALTER TABLE cities ADD CONSTRAINT cities_pk PRIMARY KEY ( city_name );
 
@@ -25,8 +19,8 @@ CREATE TABLE country (
 ALTER TABLE country ADD CONSTRAINT country_pk PRIMARY KEY ( country_name );
 
 CREATE TABLE disciplina (
-    disc_name                    VARCHAR2(50) NOT NULL,
-    sportcategory_category_name  VARCHAR2(50) NOT NULL
+    disc_name           VARCHAR2(50) NOT NULL,
+    sportcategory_name  VARCHAR2(50) NOT NULL
 );
 
 ALTER TABLE disciplina ADD CONSTRAINT disciplina_pk PRIMARY KEY ( disc_name );
@@ -38,18 +32,18 @@ CREATE TABLE event (
 ALTER TABLE event ADD CONSTRAINT event_pk PRIMARY KEY ( event_name );
 
 CREATE TABLE grantmedal (
-    disciplina_disc_name  VARCHAR2(50) NOT NULL,
-    event_event_name      VARCHAR2(50) NOT NULL,
-    athlete_athlete_name  VARCHAR2(50) NOT NULL,
-    olimpiada_olimp_year  INTEGER NOT NULL,
-    medal_color           VARCHAR2(15) NOT NULL
+    disciplina_name  VARCHAR2(50) NOT NULL,
+    event_name       VARCHAR2(50) NOT NULL,
+    athlete_name     VARCHAR2(50) NOT NULL,
+    olimpiada_year   INTEGER NOT NULL,
+    medal_color      VARCHAR2(15) NOT NULL
 );
 
 ALTER TABLE grantmedal
-    ADD CONSTRAINT grantmedal_pk PRIMARY KEY ( disciplina_disc_name,
-                                               event_event_name,
-                                               athlete_athlete_name,
-                                               olimpiada_olimp_year,
+    ADD CONSTRAINT grantmedal_pk PRIMARY KEY ( disciplina_name,
+                                               event_name,
+                                               athlete_name,
+                                               olimpiada_year,
                                                medal_color );
 
 CREATE TABLE medal (
@@ -59,8 +53,14 @@ CREATE TABLE medal (
 ALTER TABLE medal ADD CONSTRAINT medal_pk PRIMARY KEY ( color );
 
 CREATE TABLE olimpiada (
-    olimp_year INTEGER NOT NULL
+    olimp_year   INTEGER NOT NULL,
+    cities_name  VARCHAR2(50) NOT NULL
 );
+
+CREATE UNIQUE INDEX olimpiada__idx ON
+    olimpiada (
+        cities_name
+    ASC );
 
 ALTER TABLE olimpiada ADD CONSTRAINT olimpiada_pk PRIMARY KEY ( olimp_year );
 
@@ -71,27 +71,23 @@ CREATE TABLE sportcategory (
 ALTER TABLE sportcategory ADD CONSTRAINT sportcategory_pk PRIMARY KEY ( category_name );
 
 ALTER TABLE athlete
-    ADD CONSTRAINT athlete_country_fk FOREIGN KEY ( country_country_name )
+    ADD CONSTRAINT athlete_country_fk FOREIGN KEY ( country_name )
         REFERENCES country ( country_name );
 
-ALTER TABLE cities
-    ADD CONSTRAINT cities_olimpiada_fk FOREIGN KEY ( olimpiada_olimp_year )
-        REFERENCES olimpiada ( olimp_year );
-
 ALTER TABLE disciplina
-    ADD CONSTRAINT disciplina_sportcategory_fk FOREIGN KEY ( sportcategory_category_name )
+    ADD CONSTRAINT disciplina_sportcategory_fk FOREIGN KEY ( sportcategory_name )
         REFERENCES sportcategory ( category_name );
 
 ALTER TABLE grantmedal
-    ADD CONSTRAINT grantmedal_athlete_fk FOREIGN KEY ( athlete_athlete_name )
+    ADD CONSTRAINT grantmedal_athlete_fk FOREIGN KEY ( athlete_name )
         REFERENCES athlete ( athlete_name );
 
 ALTER TABLE grantmedal
-    ADD CONSTRAINT grantmedal_disciplina_fk FOREIGN KEY ( disciplina_disc_name )
+    ADD CONSTRAINT grantmedal_disciplina_fk FOREIGN KEY ( disciplina_name )
         REFERENCES disciplina ( disc_name );
 
 ALTER TABLE grantmedal
-    ADD CONSTRAINT grantmedal_event_fk FOREIGN KEY ( event_event_name )
+    ADD CONSTRAINT grantmedal_event_fk FOREIGN KEY ( event_name )
         REFERENCES event ( event_name );
 
 ALTER TABLE grantmedal
@@ -99,10 +95,14 @@ ALTER TABLE grantmedal
         REFERENCES medal ( color );
 
 ALTER TABLE grantmedal
-    ADD CONSTRAINT grantmedal_olimpiada_fk FOREIGN KEY ( olimpiada_olimp_year )
+    ADD CONSTRAINT grantmedal_olimpiada_fk FOREIGN KEY ( olimpiada_year )
         REFERENCES olimpiada ( olimp_year );
-        
-        
+
+ALTER TABLE olimpiada
+    ADD CONSTRAINT olimpiada_cities_fk FOREIGN KEY ( cities_name )
+        REFERENCES cities ( city_name );
+
+--Умова на тригер: Не дозволяє змінити колір виданої атлету медалі.
 CREATE OR REPLACE TRIGGER BlockGM 
 BEFORE UPDATE OF medal_color
 ON GrantMedal 
